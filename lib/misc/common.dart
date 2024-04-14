d3eb948fd798b6b7e6a990fd57d763495636c53d
kwstaseL <kostasel19@gmail.com>
2024-04-14 12:52:44 +0300

    Added common color constants

---
+++ lib/misc/common.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static const Color primary = Color(0xff202020);
+  static const Color accent = Color(0xffa78839);
+  static const Color background = Color(0xffffffff);
+  static const Color textHint = Color(0xFFBDBDBD);
+  static const Color divider = Color(0xFFB6B6B6);
+}
+
+class ButtonColors {
+  static const Color primary = Color(0xffa78839);
+  static const Color attention = Color(0xfff20b0b);
+}
