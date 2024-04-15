e2076e2df88e046b29b02d6af0aa21078fa8bf2a
kwstaseL <kostasel19@gmail.com>
2024-04-15 18:25:19 +0300

    Add forms and styling

--- lib/misc/common.dart
+++ lib/misc/common.dart
@@ -7,13 +7,13 @@ final poppinsFont = TextStyle(
 
 class MainColors {
   static const Color primary = Color(0xff202020);
-  static const Color accent = Color(0xffa78839);
+  static const Color accent = Color(0xfffb8000);
   static const Color background = Color(0xffffffff);
   static const Color textHint = Color(0xFFBDBDBD);
   static const Color divider = Color(0xFFB6B6B6);
 }
 
 class ButtonColors {
-  static const Color primary = Color(0xffa78839);
+  static const Color primary = Color(0xfffb7900);
   static const Color attention = Color(0xfff20b0b);
 }
--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -9,7 +9,11 @@ class AuthScreen extends StatefulWidget {
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
+enum AuthMode { login, signup }
+
 class _AuthScreenState extends State<AuthScreen> {
+  AuthMode _authMode = AuthMode.login;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +33,65 @@ class _AuthScreenState extends State<AuthScreen> {
             SvgPicture.asset(
               'assets/images/login_screen_logo.svg',
               height: 150,
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              color: MainColors.background,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: poppinsFont,
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: poppinsFont,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(ButtonColors.primary),
+                        ),
+                        child: Text(
+                          _authMode == AuthMode.login ? 'Login' : 'Signup',
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_authMode == AuthMode.login) {
+                              _authMode = AuthMode.signup;
+                            } else {
+                              _authMode = AuthMode.login;
+                            }
+                          });
+                        },
+                        child: Text(
+                          _authMode == AuthMode.login
+                              ? 'Create new account'
+                              : 'Already have an account?',
+                          style: poppinsFont.copyWith(
+                            color: MainColors.accent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
