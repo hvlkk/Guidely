b258cfd5855a9e7ca18e28618c24482262342be5
Ippo03 <ippokratispantelidis@gmail.com>
2024-04-30 01:30:19 +0300

    added tour and language model

---
+++ lib/models/entities/tour.dart
@@ -0,0 +1,35 @@
+import 'package:guidely/models/data/tour_event_location.dart';
+import 'package:guidely/models/utils/language.dart';
+
+enum TourState {
+  upcoming,
+  live,
+  past,
+}
+
+class Tour {
+  const Tour({
+    required this.title,
+    required this.area,
+    required this.description,
+    required this.duration,
+    required this.images,
+    required this.languages,
+    required this.startLocation,
+    required this.locations,
+    // required this.organizer,
+  });
+
+  final String title;
+  final String area;
+  final String description;
+  final Duration duration;
+  final List<String> images;
+  final List<Language> languages;
+  final TourEventLocation startLocation;
+  final List<TourEventLocation> locations;
+  // final User organizer;
+
+  final TourState state = TourState.upcoming;
+  final double rating = 0.0;
+}
---
+++ lib/models/utils/language.dart
@@ -0,0 +1,6 @@
+class Language {
+  const Language({required this.name, required this.code});
+
+  final String name;
+  final String code;
+}
