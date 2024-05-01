f6feb837993d6313984a42ad8967a979e9c6d751
Ippo03 <ippokratispantelidis@gmail.com>
2024-05-02 02:42:54 +0300

    tour details almost ready

--- lib/screens/main/tour_details.dart
+++ lib/screens/main/tour_details.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:guidely/misc/common.dart';
+import 'package:guidely/models/entities/review.dart';
 import 'package:guidely/models/entities/tour.dart';
+import 'package:guidely/models/entities/user.dart';
+import 'package:guidely/screens/main/host_profile.dart';
+import 'package:guidely/widgets/entities/review_list_item.dart';
 
 class TourDetailsScreen extends StatefulWidget {
   const TourDetailsScreen({
@@ -42,6 +47,37 @@ class _TourDetailsScreenState extends State<TourDetailsScreen> {
     String truncatedDescription = tour.description.length > 250
         ? '${tour.description.substring(0, 250)}...'
         : tour.description;
+
+    // TEMP CODE FOR DEBUGGING
+    List<Review> reviews = [];
+
+    Review review1 = Review(
+      grade: 5,
+      comment: 'Great tour!',
+      date: DateTime.now(),
+      user: User(
+        uid: '1',
+        username: 'John Doe',
+        email: 'ippo@example.com',
+        imageUrl: 'assets/images/user.png',
+      ),
+    );
+
+    Review review2 = Review(
+      grade: 4,
+      comment: 'Nice tour!',
+      date: DateTime.now(),
+      user: User(
+        uid: '2',
+        username: 'Jane Doe',
+        email: 'jane@example',
+        imageUrl: 'assets/images/user.png',
+      ),
+    );
+
+    reviews.add(review1);
+    reviews.add(review2);
+    // END TEMP CODE
 
     return Scaffold(
       appBar: AppBar(),
@@ -143,22 +179,64 @@ class _TourDetailsScreenState extends State<TourDetailsScreen> {
                 ],
               ),
               const SizedBox(height: 10),
-              const Card(
-                color: Color.fromARGB(239, 255, 255, 255),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'The Organizer is verified',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) {
+                        return HostProfileScreen(user: tour.organizer);
+                      },
                     ),
-                  ],
+                  );
+                },
+                child: Card(
+                  color: const Color.fromARGB(239, 255, 255, 255),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(tour.organizer.imageUrl),
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Organized by: ${tour.organizer.username}',
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Text(
+                                  'Verified Guide',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
@@ -228,7 +306,7 @@ class _TourDetailsScreenState extends State<TourDetailsScreen> {
                     ),
                 ],
               ),
-              const SizedBox(height: 50),
+              const SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () {
                   // Open the calendar to select a date
@@ -247,12 +325,85 @@ class _TourDetailsScreenState extends State<TourDetailsScreen> {
                   style: TextStyle(color: Colors.white),
                 ),
               ),
-              // NOT READY CODE
-              const SizedBox(height: 10),
-              const Text('PROOF THAT SCROLLING WORKS'),
-              const SizedBox(height: 10),
-              Text(tour.description),
-              // Text(tour.startLocation.toString()),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Text(
+                    'Roadmap',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  const Icon(Icons.flag, color: Colors.red),
+                  Column(
+                    children: [
+                      const Text(
+                        'Meeting Point',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tour.startLocation.address,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(width: 10),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 300, // Adjust the height as needed
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(
+                          tour.startLocation.latitude,
+                          tour.startLocation
+                              .longitude), // Initial position (San Francisco)
+                      zoom: 12, // Zoom level
+                    ),
+                    markers: {
+                      Marker(
+                        markerId: const MarkerId('marker_1'),
+                        position: LatLng(tour.startLocation.latitude,
+                            tour.startLocation.longitude), // Marker position
+                        infoWindow: const InfoWindow(
+                            title: 'Athens Center'), // Info window
+                      ),
+                    },
+                    onMapCreated: (GoogleMapController controller) {
+                      // Controller is ready
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Reviews',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 300,
+                child: ListView.builder(
+                  itemCount: reviews.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: ReviewListItem(review: reviews[index]),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
