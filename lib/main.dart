117cd6d5559d3560b7f1fbd3fbcf46cb75ad3f57
kwstaseL <kostasel19@gmail.com>
2024-05-23 16:42:38 +0300

    Testing for notifications

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:guidely/utils/location_finder.dart';
@@ -18,6 +19,7 @@ void main() async {
 
   // Request location permission
   await LocationFinder.requestLocationPermission();
+  _requestNotificationPermissions();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
@@ -25,6 +27,25 @@ void main() async {
       child: MainApp(),
     ),
   );
+}
+
+Future<void> _requestNotificationPermissions() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Request permission for iOS
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
 }
 
 class MainApp extends StatefulWidget {
